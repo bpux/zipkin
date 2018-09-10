70631979856b49f8ba63e7694e66314ece961444
Ke Xu <ke.xu@salesforce.com>
2018-09-10 11:52:11 -0700

    add

--- zipkin-ui/js/skew.js
+++ zipkin-ui/js/skew.js
@@ -1,4 +1,17 @@
 /*
+ * Copyright 2015-2018 The OpenZipkin Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+/*
  * Convenience type representing a tree. This is here because multiple facets in zipkin require
  * traversing the trace tree. For example, looking at network boundaries to correct clock skew, or
  * counting requests imply visiting the tree.
